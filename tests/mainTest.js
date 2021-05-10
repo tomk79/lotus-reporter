f004ab733471ba76e2272fe8ba7b73bc814da86a
Tomoya Koyanagi <tomk79@gmail.com>
2021-05-11 00:20:41 +0900

    npm install @tomk79/rotus-crawler

--- tests/mainTest.js
+++ tests/mainTest.js
@@ -1,5 +1,18 @@
 const assert = require('assert');
 const fsEx = require('fs-extra');
+const LotusCrawler = require('@tomk79/lotus-crawler');
+const lotus = new LotusCrawler({
+	user_agent: 'Mozilla/5.0 Test Agent',
+	ranges: [
+		'http://127.0.0.1:3000/',
+		'http://127.0.0.1:3001/',
+		'http://127.0.0.1:3002/'
+	],
+	db: {
+		prefix: 'LotusCrawlerTest',
+	},
+});
+let serverProc;
 
 describe('Starting test server', function() {
 
@@ -20,6 +33,36 @@ describe('Starting test server', function() {
 		}, 5000);
 	});
 
+});
+
+describe('Crawling', function() {
+
+	it("Creating Instance", function(done) {
+		this.timeout(60*1000);
+		assert.equal(typeof(lotus), typeof({}));
+		done();
+	});
+
+	it("Adding Target URL", function(done) {
+		this.timeout(60*1000);
+		lotus.add_target_url('http://127.0.0.1:3000/')
+			.then(result => {
+				assert.equal(result, true);
+				done();
+			});
+	});
+
+	it("Crawling", function(done) {
+		this.timeout(60*1000);
+		lotus.crawl()
+			.then(() => {
+				done();
+			})
+			.catch((message) => {
+				console.error(message);
+				done();
+			});
+	});
 });
 
 describe('Test', function() {
